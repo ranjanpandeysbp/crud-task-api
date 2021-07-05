4d9b45482d84360f9ab55cb8356eb779aa08c4e7
skillsapphire <skillsapphire@gmail.com>
2021-07-05 17:48:35 +0200

    delete all task if a tasklist is deleted

--- app.js
+++ app.js
@@ -102,11 +102,21 @@ app.patch('/tasklists/:tasklistId', (req, res) => {
 
 //Delete a tasklist by id
 app.delete('/tasklists/:tasklistId', (req, res) => {
-    TaskList.findByIdAndDelete(req.params.tasklistId)
+
+    //Delete all tasks withing a taklist if that tasklist is deleted
+    const deleteAllContainingTask = (taskList) => {
+        Task.deleteMany({ _taskListId: req.params.tasklistId })
+            .then(() => { return taskList })
+            .catch((error) => { console.log(error) });
+    };
+
+    const responseTaskList = TaskList.findByIdAndDelete(req.params.tasklistId)
         .then((taskList) => {
-            res.status(201).send(taskList)
+            deleteAllContainingTask(taskList);
         })
         .catch((error) => { console.log(error) });
+
+    res.status(200).send(responseTaskList);
 });
 
 /* CRUD operation for Task, a task should always belong to a TaskList*/
